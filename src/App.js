0c124aad4328ce69918218d93abdf24e5a4591aa
jakxc <jack.x.chen@outlook.com>
2023-02-11 11:23:56 +1000

    State and win condition update
    - Added dice array state
    - Added win con state
    - Minor styling updates

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
 import React from "react";
 import Die from "./components/Die"
 import {nanoid} from 'nanoid';
+import Confetti from 'react-confetti'
 
 function App() {
-  const [diceArray, setDiceArray] = React.useState(allNewDice())
+  const [diceArray, setDiceArray] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(()=> {
+    const allHeld = diceArray.every(item => item.isHeld);
+    const firstValue = diceArray[0].value;
+    const allSameValue = diceArray.every(item => item.value === firstValue);
+    
+    if (allHeld && allSameValue)
+    {
+      setTenzies(true);
+    }
+  }, [diceArray])
+
+  function generateNewDie()
+  {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false
+    }
+  }
 
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push({
-          id: nanoid(),
-          value: Math.ceil(Math.random() * 6), 
-          isHeld: false
-        })
+        newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice()
   {
-    setDiceArray(allNewDice());
+    if (!tenzies)
+    {
+    setDiceArray(prevArray => prevArray.map(item => 
+      {
+        return item.isHeld ? item : generateNewDie();
+      }))
+    } else 
+    {
+      setTenzies(false);
+      setDiceArray(allNewDice());
+    }
+  }
+
+  function holdDice(id)
+  {
+    setDiceArray(prevArray => prevArray.map(item => 
+      {
+        return item.id === id ? 
+        {...item, isHeld: !item.isHeld} : item
+      }))
   }
 
   const diceElements = diceArray.map(item => 
-    <Die id={item.id} value={item.value} isHeld={item.isHeld} 
+    <Die 
+      id={item.id} 
+      value={item.value} 
+      isHeld={item.isHeld}
+      handleClick={() => holdDice(item.id)}
   />)
 
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="button-roll" onClick={rollDice}>Roll</button>
+      <button 
+        className="button-roll" 
+        onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   );
 }
