c34162cd5d1a04b178813cac83e51dc383c4c11b
jakxc <jack.x.chen@outlook.com>
2023-02-16 22:06:27 +1000

    Added faces to Dice component

--- src/components/Die.js
+++ src/components/Die.js
@@ -3,12 +3,77 @@ import React from "react";
 function Die(props)
 {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#59E391" : "tomato"
     }
     return (
-        <div className="die-face" style={styles} onClick={props.handleClick}>
-            <h2 className="die-num">{props.value}</h2>
-        </div>
+        <>
+            {props.value === 1 &&
+                <div class="dice first-face" style={styles} onClick={props.handleClick}>
+                    <span class="dot"></span>
+                </div>
+            }
+
+            {props.value === 2 && 
+                <div class="dice second-face" style={styles} onClick={props.handleClick}>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            }
+
+            {props.value === 3 && 
+                <div class="dice third-face" style={styles} onClick={props.handleClick}>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </div>
+            }
+
+            {props.value === 4 && 
+                <div class="dice fourth-face" style={styles} onClick={props.handleClick}>
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
+            }
+
+            {props.value === 5 && 
+                <div class="dice fifth-face" style={styles} onClick={props.handleClick}>
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    
+                    <div class="column">
+                        <span class="dot"></span>
+                    </div>
+                    
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
+            }
+
+            {props.value === 6 && 
+                <div class="dice sixth-face" style={styles} onClick={props.handleClick}>
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                    <div class="column">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                            <span class="dot"></span>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
 
